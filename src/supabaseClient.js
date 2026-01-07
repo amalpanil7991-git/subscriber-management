714ad1cdc0362ce00ac692bc3f04c2d6f3c2dfab
amalpanil7991 <amalpanil7991@gmail.com>
2026-01-07 10:57:06 +0530

    Supabase DB URLS added

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'project_one/-/tree/development'
-const supabaseAnonKey = 'sb_publishable_bJKUqc2pAMpB3HTF32xbuQ_oOuNuy8V'
+const supabaseUrl = 'https://iuhjkndvwaomqvvjsogh.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1aGprbmR2d2FvbXF2dmpzb2doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3NDA4MjYsImV4cCI6MjA4MzMxNjgyNn0.Hx9EZgxsB1cDlQD_-dEBVVZ4w4K9FdRrWpOcEv7hvH4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
